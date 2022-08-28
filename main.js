081fdcf63d4f2975cde47731fbc074e54b5b411a
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-08-28 11:09:44 -0300

    Finalizing

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import server from './models/server_models.js'
 import express from 'express'
 import router from './routes/routes.js'
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
 
 // Read JSON
@@ -14,10 +13,13 @@ server.app.use(
 
 server.app.use(express.json())
 
+//Test Database
+await prisma.$connect().catch((err)=>{
+    console.log(`Error trying to connect to the database: ${err}`)
+})
+
 // Define the application routes
 server.app.use('/api', router)
-
-//Test Database
 
 // Init server
 server.app.listen(server.serverPort, () => {
--- models/server_models.js
+++ models/server_models.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { Sequelize } from 'sequelize'
 
 const server = {
     app: express(),
